7f9b3a0d920676f8337f4c79b5359d8a0b7af426
chetanmelavanki <chetanmelavanki2000@gmail.com>
2022-04-17 16:54:20 +0530

    updating jdbc

--- jdbc-demo/src/main/java/com/xworkz/jdbcdemo/Runner.java
+++ jdbc-demo/src/main/java/com/xworkz/jdbcdemo/Runner.java
@@ -16,8 +16,8 @@ public class Runner {
 		
 		
 		insertMovie();
-//		updateMovie();
-//		deleteMovie();
+		updateMovie();
+		deleteMovie();
 	}
 
 	private static void insertMovie() {
@@ -78,92 +78,92 @@ public class Runner {
 		}
 	}
 	
-//	private static void updateMovie() {
-//		Scanner sc1=new Scanner(System.in);
-//		
-//		System.out.println("enter movie name to update ");
-//		String name=sc1.next();
-//		
-//		System.out.println("enter a rating to update ");
-//		double rating=sc1.nextDouble();
-//		
-//		System.out.println("enter a language to update");
-//		String language=sc1.next();
-//		
-//		System.out.println("enter a budget to uodate");
-//		long budget=sc1.nextLong();
-//		
-//		System.out.println("enter a id to update all the the deatils");
-//		int id=sc1.nextInt();
-//		
-//		Connection connection=null;
-//		Statement statement=null;
-//		try {
-//			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jan6", "root", "ROOT");
-//			System.out.println("Connection created");
-//			
-//			statement=connection.createStatement();
-//			System.out.println("statement is created");
-//			
-//			statement.execute("UPDATE movie SET name=('"+name+"'),rating=("+rating+"),language=('"+language+"'),budget=("+budget+") where id=("+id+")");
-//			System.out.println("successfully updated");
-//			
-//		}catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		finally {
-//			//step 6 
-//			//close the costly resources
-//			try {
-//				if(statement !=null )
-//					statement.close();
-//				if(connection != null)
-//					connection.close();
-//					
-//			} catch (Exception e2) {
-//				e2.printStackTrace();
-//			}
-//		}
-//		
-//	}
+	private static void updateMovie() {
+		Scanner sc1=new Scanner(System.in);
+		
+		System.out.println("enter movie name to update ");
+		String name=sc1.next();
+		
+		System.out.println("enter a rating to update ");
+		double rating=sc1.nextDouble();
+		
+		System.out.println("enter a language to update");
+		String language=sc1.next();
+		
+		System.out.println("enter a budget to uodate");
+		long budget=sc1.nextLong();
+		
+		System.out.println("enter a id to update all the the deatils");
+		int id=sc1.nextInt();
+		
+		Connection connection=null;
+		Statement statement=null;
+		try {
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jan6", "root", "ROOT");
+			System.out.println("Connection created");
+			
+			statement=connection.createStatement();
+			System.out.println("statement is created");
+			
+			statement.execute("UPDATE movie SET name=('"+name+"'),rating=("+rating+"),language=('"+language+"'),budget=("+budget+") where id=("+id+")");
+			System.out.println("successfully updated");
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			//step 6 
+			//close the costly resources
+			try {
+				if(statement !=null )
+					statement.close();
+				if(connection != null)
+					connection.close();
+					
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+	}
 	
-//	private static void deleteMovie() {
-//		Scanner sc=new Scanner(System.in);
-//	
-//		System.out.println("enter id to delete all the details");
-//		int id=sc.nextInt();
-//		
-//		Connection connection=null;
-//		Statement statement=null;
-//		try {
-//			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jan6", "root", "ROOT");
-//			System.out.println("Connection created");
-//			
-//			statement=connection.createStatement();
-//			System.out.println("statement is created");
-//			
-//			statement.execute("DELETE FROM movie where id=("+id+")");
-//			System.out.println("successfully deleted");
-//			
-//		}catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		finally {
-//			//step 6 
-//			//close the costly resources
-//			try {
-//				if(statement !=null )
-//					statement.close();
-//				if(connection != null)
-//					connection.close();
-//					
-//			} catch (Exception e2) {
-//				e2.printStackTrace();
-//			}
-//		}
-//		
-//	}
-//	
+	private static void deleteMovie() {
+		Scanner sc=new Scanner(System.in);
+	
+		System.out.println("enter id to delete all the details");
+		int id=sc.nextInt();
+		
+		Connection connection=null;
+		Statement statement=null;
+		try {
+			connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/jan6", "root", "ROOT");
+			System.out.println("Connection created");
+			
+			statement=connection.createStatement();
+			System.out.println("statement is created");
+			
+			statement.execute("DELETE FROM movie where id=("+id+")");
+			System.out.println("successfully deleted");
+			
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			//step 6 
+			//close the costly resources
+			try {
+				if(statement !=null )
+					statement.close();
+				if(connection != null)
+					connection.close();
+					
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		
+	}
+	
 	
 
 }
